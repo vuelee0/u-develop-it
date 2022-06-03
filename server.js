3c5f7de3c4c58627f1582740b81f46b640f577ad
Vue Lee <vuelee09@gmail.com>
2022-06-03 02:36:21 -0500

    add CRUD routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // import express
 const express = require('express');
 const mysql = require('mysql2');
+const inputCheck = require('./utils/inputCheck');
 
 // PORT designation and app expression
 const PORT = process.env.PORT || 3001;
@@ -77,6 +78,29 @@ app.delete('/api/candidate/:id', (req, res) => {
                 id: req.params.id
             });
         }
+    });
+});
+
+// Create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+        VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
     });
 });
 
