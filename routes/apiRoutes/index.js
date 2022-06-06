591f7be69e58eea5d825a93e0c03ca886e42a15d
Vue Lee <vuelee09@gmail.com>
2022-06-06 17:09:47 -0500

    create query for vote total and api route

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+
+
 router.use(require('./candidateRoutes'));
 router.use(require('./partyRoutes'));
 router.use(require('./voterRoutes'));
+router.use(require('./voteRoutes'));
+
+
 
 module.exports = router;
